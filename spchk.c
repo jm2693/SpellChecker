4993143031191b32d516300f2df98bf77d973420
Daniel <dal279@scarletmail.rutgers.edu>
2024-03-11 09:37:42 -0400

    includes

--- spchk.c
+++ spchk.c
@@ -1,8 +1,11 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
-#define DEBUG 1
+#define DEBUG=1
 
 int main (int args, char** argv){
-    
+
 }
