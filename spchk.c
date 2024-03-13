a82d94fae26e6811d652c412abbd427e6d528f56
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-03-12 23:02:47 -0400

    basic recurisve file search

--- spchk.c
+++ spchk.c
@@ -3,8 +3,45 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
-
+#include <dirent.h>
 #define DEBUG 1
+
+void dict_arr () {
+
+
+}
+
+
+
+
+void list_files (char* filename) {
+    DIR *dir = opendir(filename);
+    if (dir == NULL) return;
+
+    typedef struct dirent dirent;
+    dirent* entity;
+    entity = readdir(dir);
+
+    while (entity != NULL) {
+        if (entity->d_type == DT_DIR) {
+            char path[200] = { '\0' };
+            strcat(path, filename);
+            strcat(path, '/');
+            strcat(path, entity->d_name);
+            list_files(path);
+        }
+    }
+
+    closedir(dir);
+}
+
+
+
+
+void check_for_word() {
+
+}
+
 
 int main (int argc, char** argv){
     char *fname = argv[1];
