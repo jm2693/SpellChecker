e2021dd11eb315cf824ee8b67fa5dbeef969f4ff
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-03-17 05:20:14 -0400

    i think dict_arr is done

--- spchk.c
+++ spchk.c
@@ -5,24 +5,88 @@
 #include <string.h>
 #include <dirent.h>
 #include <ctype.h>
+
 #define DEBUG 1
+#define DICT_LINES 10000                    // initial num of lines assumed to be 10000
+#define WORD_LENGTH 100                     // initial word length assumed to be 99 chars + null terminator 
 
-typedef struct word {                       // creates a char* struct, as to not use a char[][] and instead use a word* for a string array
+typedef struct Word {                       // creates a char* struct, as to not use a char[][] and instead use a word* for a string array
     char *word;
-} word;
+} Word;
 
-word* dict_arr (char* dict_file, int* word_num) {   // dict_arr will take in the dictionary pathname and return an pointer to a word array (array of strings)
+Word* dict_arr (char* dict_file, int* word_num) {   // dict_arr will take in the dictionary pathname and return an pointer to a word array (array of strings)
 
     int fd = open(dict_file, O_RDONLY);     // opens the dictionary file using its pathname 
     if (fd < 0) {
         perror("Error: ");                  // prints error using msg and errno 
-        return EXIT_FAILURE;                // if open fails, returns error
+        return NULL;                // if open fails, returns error
     }
-    word *dictionary_start = NULL;          // a pointer to the word array (what the function will return)
-    *word_num = 0;
 
 
+
+    int lines_num = DICT_LINES;                  // initial assumed number of lines
+    char word_buffer[WORD_LENGTH];               // essentially a blank string. acts as our buffer
+    *word_num = 0;                               // the current numbers of words in the dictionary is 0
+
+    Word *dictionary_start = (Word*)malloc(lines_num * sizeof(Word));   // a pointer to the word array (what the function will return)
+    if (dictionary_start == NULL) {                                     // in case allocation fails
+        close(fd);
+        perror("Error: ");
+        return NULL;
+    }
+
+
+    ssize_t bytes_read;                     // number of bytes read at a time
+    while((bytes_read = read(fd, word_buffer, sizeof(word_buffer))) > 0) {  // reads in WORD_LENGTH bytes at a time into the word_buffer
+
+        char* start = word_buffer;          // string to be copied into dictionary_start array
+        char* end = start;                  // used to locate the end of the word
+
+        do {
+            if (*end == '\n') {             // once end locates the newline operator
+                *end = '\0';                // it replaces it with null terminator to denote the end of a word
+
+                dictionary_start[*word_num].word = strdup(start);   // word then gets copied into dictionary_start (using strdup over strcpy because it is more memory safe)
+                if (dictionary_start[*word_num].word == NULL) {     // in case allocation fails
+ 
+                    for (int i = 0; i < *word_num; i++) {
+                        free(dictionary_start[i].word);
+                    }
+                    free(dictionary_start);
+                    close(fd);
+                    perror("Error: ");
+                    return NULL;
+                }
+
+                (*word_num)++;                                      // number of words in dict goes up
+
+                if ((*word_num) >= lines_num) {                     // in case there are more words than initial assumption
+                    lines_num *= 2;
+
+                    Word *bigger_dict_arr = realloc(dictionary_start, lines_num * sizeof(Word));
+                    if (bigger_dict_arr == NULL) {
+                        for (int i = 0; i < *word_num; i++) {
+                            free(dictionary_start[i].word);
+                        }
+                        free(dictionary_start);
+                        close(fd);
+                        perror("Error: ");
+                        return NULL;
+                    }
+                    dictionary_start = bigger_dict_arr;
+                }
+                start = end + 1;
+            }
+
+            bytes_read -= (word_buffer - start);            // in case something goes wrong and 
+            if (bytes_read > 0) {
+                memmove(word_buffer, start, bytes_read);
+            }
+
+        } while ((*end != '\n') && (end != NULL));     
+    }
     close(fd);                              // anything opened must be closed when done with use
+    return dictionary_start;
 }
 
 
@@ -44,7 +108,7 @@ void file_search (char* filename) {          // function to recursively search f
         if (entity->d_type == DT_DIR) {      // checks if the file is a directory 
             char path[200] = { '\0' };       // creates path for the file as a string
             strcat(path, filename);          // concatonates the currrent file to the path variable
-            strcat(path, '/');               // '/' to indicate new dir
+            strcat(path, "/");               // '/' to indicate new dir
             strcat(path, entity->d_name);    // 
             file_search(path);               // recursive search
         }
@@ -70,7 +134,7 @@ void return_error() {
 int main (int argc, char** argv){
 
     if (argc < 3) {
-        printf("Use of %s: <dictionary_path> <file1> <file2> ...");
+        printf("Use of %s: <dictionary_path> <file1> <file2> ...", argv[0]);
         return EXIT_FAILURE;
     }
 
